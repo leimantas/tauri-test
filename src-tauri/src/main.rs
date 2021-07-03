def676358a02a7746b369d8da42175b58b8ef4b4
Eimantas <eimantas.lapinskas@gmail.com>
2021-07-03 22:55:27 +0200

    first commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+use tauri::{Menu, MenuItem};
+
+fn main() {
+  let menu = Menu::new().add_native_item(MenuItem::Quit);
+
+  tauri::Builder::default()
+    .menu(menu)
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-red-500">
+      <header className="p-8">
+        <p className="text-2xl text-center text-white">Hello Vite + React!</p>
+        <input name="input" className="border-4 border-red-200" autoFocus />
+      </header>
+    </div>
+  );
+}
+
+export default App;

e9991ae436aaa065ce0028118a27e42c035cc6c0
Eimantas <eimantas.lapinskas@gmail.com>
2021-07-07 23:14:17 +0300

    taiwlind jit mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import TestComponent from "components/Test";
 
 const App = () => {
   return (
-    <div className="bg-red-500">
+    <div className="bg-blue-500">
       <header className="p-8">
         <p className="text-2xl text-center text-white">Hello Vite + React!</p>
         <input name="input" className="border-4 border-red-200" autoFocus />

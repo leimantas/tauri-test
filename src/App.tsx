c151e68a1d9af1f16ee02342070c82dd4caf4cc9
Eimantas <eimantas.lapinskas@gmail.com>
2021-07-07 20:15:47 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import TestComponent from "components/Test";
 
-function App() {
+const App = () => {
   return (
     <div className="bg-red-500">
       <header className="p-8">
         <p className="text-2xl text-center text-white">Hello Vite + React!</p>
         <input name="input" className="border-4 border-red-200" autoFocus />
+        <TestComponent />
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestComponent = () => {
+  return <h2 className="py-3 text-xl text-yellow-50">Test</h2>;
+};
+
+export default TestComponent;
